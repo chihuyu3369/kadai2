7795379534f7dfdb1dfa6d6bb7c4dd62470a5227
EC2 Default User <ec2-user@ip-172-31-33-114.ap-northeast-1.compute.internal>
2023-04-25 13:00:53 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
      if @book.save
@@ -14,39 +14,39 @@ class BooksController < ApplicationController
       render :index
      end
   end
-  
+
   def show
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
-  
+
   def view
   @book = Book.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-      @book = Book.new(book_params)
-     if @book.save
+      @book = Book.find(params[:id])
+     if @book.update(book_params)
       flash[:success] = "Book was successfully updated."
       redirect_to book_path(@book.id)
      else
       @books = Book.all
-      render :index
+      render :edit
      end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])  # データ（レコード）を1件取得
     @book.destroy  # データ（レコード）を削除
-    redirect_to books_path  # 投稿一覧画面へリダイレクト  
+    redirect_to books_path  # 投稿一覧画面へリダイレクト
   end
-  
+
   private
    def book_params
      params.require(:book).permit(:title, :body)
    end
-  
+
 end
